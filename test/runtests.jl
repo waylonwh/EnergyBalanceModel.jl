a1a1478489136f00c02509966b360b031d6928d2
Waylon Wu <waylon_wu@outlook.com>
2026-02-28 11:48:14 +1100

    update tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,33 +1,42 @@
 using EnergyBalanceModel, Test
 
-import EnergyBalanceModel.Infrastructure:AbstractModel
+import EnergyBalanceModel.Infrastructure: ClassicModel, MIZModel
+
+st = SpaceTime{sin}(180, 2000, 30)
+forcing = Forcing(0.0)
+
+mizpar = default_parameters(miz)
+T = fill(17.0, st.nx)
+mizinit = Collection{Vec}(
+    :Ei => zeros(st.nx),
+    :Ew => mizpar.cw * T,
+    :h => zeros(st.nx),
+    :D => zeros(st.nx),
+    :Tg => T,
+) # Collection
+
+clapar = default_parameters(classic)
+clainit = Collection{Vec}(
+    :E => clapar.cw * T,
+    :Tg => T
+)
+
+lastyear_hemi_mean(sols::Solutions, var::Symbol)::Float64 =
+    hemispheric_mean(getproperty(sols.annual.avg, var)[sols.spacetime.dur], sols.spacetime.x)
+
+@testset "Code can run" begin
+    global mizsols = integrate(miz, st, forcing, mizpar, mizinit; updatefreq=Inf)
+    global clasols = integrate(classic, st, forcing, clapar, clainit; updatefreq=Inf)
+    @test mizsols isa Solutions{MIZModel,sin,true}
+    @test clasols isa Solutions{ClassicModel,sin,true}
+end # @testset begin
 
 @testset "Test for annual hemispheric means" begin
-    st = SpaceTime(180, 2000, 20)
-    forcing = Forcing(0.0)
+    @test lastyear_hemi_mean(mizsols, :T) - lastyear_hemi_mean(clasols, :T) < 1.0
+    @test lastyear_hemi_mean(mizsols, :E) - lastyear_hemi_mean(clasols, :E) < 10.0
+end # @testset begin
 
-    mizpar = default_parameters(miz)
-    T = fill(17.0, st.nx)
-    mizinit = Collection{Vec}(
-        :Ei => zeros(st.nx),
-        :Ew => T .* mizpar.cw,
-        :h => zeros(st.nx),
-        :D => zeros(st.nx),
-        :Tg => T,
-    ) # Collection
-
-    clapar = default_parameters(classic)
-    clainit = Collection{Vec}(
-        :E => clapar.cw * T,
-        :Tg => T
-    )
-
-    mizsols = integrate(miz, st, forcing, mizpar, mizinit; updatefreq=Inf)
-    clasols = integrate(classic, st, forcing, clapar, clainit; updatefreq=Inf)
-
-    (lastyear_hemi_mean(sols::Solutions{<:AbstractModel,F,C}, var::Symbol)::Float64) where {F, C} =
-        hemispheric_mean(getproperty(sols.annual.avg, var)[sols.spacetime.dur], sols.spacetime.x)
-
-    @test lastyear_hemi_mean(mizsols, :T) - lastyear_hemi_mean(clasols, :T) < 0.1
-    @test lastyear_hemi_mean(mizsols, :E) - lastyear_hemi_mean(clasols, :E) < 1.0
+@testset "Check the solutions include T0" begin
+    @test hasproperty(mizsols.raw, :T0)
+    @test hasproperty(clasols.raw, :T0)
 end # @testset begin
